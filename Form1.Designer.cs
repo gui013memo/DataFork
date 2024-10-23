4129745807b1e99ee10ee46378a7caaca5b85c41
Guiherme Oliveira <guilhermeoliveira36023@gmail.com>
2024-10-22 23:23:01 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace DataFork
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(12, 49);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(347, 143);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "VBKABP21110004720S1070TB1B1EK1617111BBBBCCC\r\n[)>\u001e06\u001dVT6MP\u001dP2410007010\u001dT230627L1C1A0026\u001d\u001e\u00040026\r\n";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(96, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(177, 32);
+            label1.TabIndex = 1;
+            label1.Text = "Ultimas leituras";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(96, 214);
+            label2.Name = "label2";
+            label2.Size = new Size(186, 32);
+            label2.TabIndex = 3;
+            label2.Text = "Enviado ao TMS";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(12, 249);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(347, 143);
+            textBox2.TabIndex = 2;
+            textBox2.Text = "VBKABP21110004720S1070TB1B1EK1617111BBBBCCC\r\n[)>\u001e06\u001dVT6MP\u001dP2410007010\u001dT230627L1C1A0026\u001d\u001e\u00040026\r\n";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(98, 423);
+            label3.Name = "label3";
+            label3.Size = new Size(182, 32);
+            label3.TabIndex = 5;
+            label3.Text = "Enviado ao SQS";
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(12, 458);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(347, 143);
+            textBox3.TabIndex = 4;
+            textBox3.Text = "VBKABP21110004720S1070TB1B1EK1617111BBBBCCC\r\n[)>\u001e06\u001dVT6MP\u001dP2410007010\u001dT230627L1C1A0026\u001d\u001e\u00040026\r\n";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 729);
+            Controls.Add(label3);
+            Controls.Add(textBox3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace DataFork
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
